05c810f857914b3ffe5f73ac8e8296f07696c8de
iftakher99 <19tonmoy99@gmail.com>
2022-10-17 21:34:23 +0600

    test

---
+++ Phitron-c/problem3.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main (){
+int natural_number ;
+printf("Write one posetive integer to make it double :");
+scanf("%d",&natural_number);
+
+if(natural_number>0){
+
+ printf("%d",natural_number*2);
+}else {
+printf("Not a Positive intiger");
+}
+return 0;
+}
